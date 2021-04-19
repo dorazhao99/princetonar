0c1e4564fa453a6c86ce25dabf15e6480aceb26f
Dora Zhao <dorothyz@princeton.edu>
2021-04-18 20:04:15 -0400

    reorganize files

---
+++ public/252f366e-0155c1a5641b1f4dbc12.js
@@ -0,0 +1,2 @@
+(self.webpackChunkprinceton_ar=self.webpackChunkprinceton_ar||[]).push([[228],{2775:function(t,n,c){"use strict";c.d(n,{_YL:function(){return a}});var r=c(1046);function a(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6 0 1.01-.25 1.97-.7 2.8l1.46 1.46C19.54 15.03 20 13.57 20 12c0-4.42-3.58-8-8-8zm0 14c-3.31 0-6-2.69-6-6 0-1.01.25-1.97.7-2.8L5.24 7.74C4.46 8.97 4 10.43 4 12c0 4.42 3.58 8 8 8v3l4-4-4-4v3z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-0155c1a5641b1f4dbc12.js.map
